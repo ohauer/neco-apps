f6edcce94ce0d6bc1b1c35477c877a0702163025
Daichi Sakaue <daichi-sakaue@cybozu.co.jp>
2021-09-14 19:08:24 +0900

    team-management: add app-ept and dev-ept namespaces
    Signed-off-by: Daichi Sakaue <daichi-sakaue@cybozu.co.jp>

--- team-management/template/add_team.sh
+++ team-management/template/add_team.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # usage: ./add_team.sh team
-yq eval ". *+ {\"namespaces\": {\"$1\": {}}}" settings.json -j > /tmp/settings.json
+./bin/yq eval ". *+ {\"namespaces\": {\"$1\": {}}}" settings.json -j > /tmp/settings.json
 mv /tmp/settings.json settings.json
 make format-settings > /dev/null
