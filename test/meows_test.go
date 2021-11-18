d9673d34e742a45745f802ca253a39144e0d9660
Masayuki Ishii <47380942+masa213f@users.noreply.github.com>
2021-11-18 18:30:36 +0900

    Allow runner pods to access kube-apiserver (#2013)
    * Allow runner pods to access kube-apiserver
    
    Signed-off-by: Masayuki Ishii <masa213f@gmail.com>

--- test/meows_test.go
+++ test/meows_test.go
@@ -2,20 +2,21 @@ package test
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
 )
 
 const (
 	meowsRunnerNS   = "meows-runner"
 	meowsSecretFile = "meows-secret.json"
+	tenantDevNS     = "dev-maneki"
+	tenantTeam      = "maneki"
 )
 
 func meowsDisabled() bool {
@@ -28,17 +29,25 @@ func prepareMeows() {
 		if meowsDisabled() {
 			Skip("meows is disabled")
 		}
+		runnerPoolName := genRunnerPoolName()
 
-		hostname, err := os.Hostname()
-		Expect(err).NotTo(HaveOccurred())
-
+		By("creating a RunnerPool in the " + meowsRunnerNS)
 		var buf bytes.Buffer
 		tpl := template.Must(template.ParseFiles(filepath.Join(".", "testdata", "meows-runnerpool.tmpl.yaml")))
 		tpl.Execute(&buf, map[string]string{
-			"RunnerPoolName": "runnerpool-" + hostname,
+			"RunnerPoolName": runnerPoolName,
+			"Namespace":      meowsRunnerNS,
 		})
+		_, stderr, err := ExecAtWithInput(boot0, buf.Bytes(), "kubectl", "apply", "-f", "-")
+		Expect(err).NotTo(HaveOccurred(), "stderr: %s", stderr)
 
-		_, stderr, err := ExecAtWithInput(boot0, buf.Bytes(), "kubectl", "apply", "--namespace", meowsRunnerNS, "-f", "-")
+		By("creating a RunnerPool in a tenant namespace (" + tenantDevNS + ") as a tenant team member")
+		tpl = template.Must(template.ParseFiles(filepath.Join(".", "testdata", "meows-runnerpool.tmpl.yaml")))
+		tpl.Execute(&buf, map[string]string{
+			"RunnerPoolName": runnerPoolName,
+			"Namespace":      tenantDevNS,
+		})
+		_, stderr, err = ExecAtWithInput(boot0, buf.Bytes(), "kubectl", "apply", "-f", "-", "--as=test", "--as-group="+tenantTeam, "--as-group=system:authenticated")
 		Expect(err).NotTo(HaveOccurred(), "stderr: %s", stderr)
 	})
 }
@@ -68,47 +77,73 @@ func testMeows() {
 		if meowsDisabled() {
 			Skip("meows is disabled")
 		}
+		runnerPoolName := genRunnerPoolName()
+		checkRunnerPool(runnerPoolName, meowsRunnerNS)
+		checkRunnerPool(runnerPoolName, tenantDevNS)
 
-		hostname, err := os.Hostname()
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(func() error {
-			return checkDeploymentReplicas("runnerpool-"+hostname, "meows-runner", 1)
-		}).Should(Succeed())
+		By("checking network policy for runner pods in the " + meowsRunnerNS)
+		connectionShouldBeDenied(meowsRunnerNS, "deploy/"+runnerPoolName, "http://argocd-server.argocd.svc")
+		connectionShouldBeDenied(meowsRunnerNS, "deploy/"+runnerPoolName, "http://slack-agent.meows.svc")
+		connectionShouldBeDenied(meowsRunnerNS, "deploy/"+runnerPoolName, "https://kubernetes.default.svc")
 
-		By("checking that runner pods become online")
-		Eventually(func() error {
-			result, err := queryMetrics(MonitoringLargeset, `count(meows_runner_online)`)
-			if err != nil {
-				return err
-			}
-			if len(result.Data.Result) <= 0 {
-				return fmt.Errorf("no count metrics is retrieved")
-			}
-			onlineRunners := int(result.Data.Result[0].Value)
-			if onlineRunners != 1 {
-				return fmt.Errorf("there is not an online runner pod, the metrics of count(meows_runner_online) is %d", onlineRunners)
-			}
-			return nil
-		}).Should(Succeed())
-
-		By("getting runner pod list")
-		runnerPodList := new(corev1.PodList)
-		stdout := ExecSafeAt(boot0, "kubectl", "get", "pods", "-n", meowsRunnerNS, "-l=app.kubernetes.io/name=meows,app.kubernetes.io/component=runner", "-o=json")
-		err = json.Unmarshal(stdout, runnerPodList)
-		Expect(err).ShouldNot(HaveOccurred())
-
-		By("accessing to private IP: should be deny")
-		stdout, stderr, err := ExecAt(boot0, "kubectl", "exec", "-n", runnerPodList.Items[0].Namespace, runnerPodList.Items[0].Name, "--", "curl", "testhttpd.test-netpol", "-m", "5")
-		Expect(err).To(HaveOccurred(), "stdout: %s, stderr: %s", stdout, stderr)
-
-		By("accessing to slack-agent: should be allow")
-		ExecSafeAt(boot0, "kubectl", "exec", "-n", runnerPodList.Items[0].Namespace, runnerPodList.Items[0].Name, "--", "/usr/local/bin/meows", "slackagent", "send", runnerPodList.Items[0].Name, "success", "-s", "http://slack-agent.meows.svc", "-c", "test1", "-n", runnerPodList.Items[0].Namespace)
-		Eventually(func() error {
-			stdout, stderr, err := ExecAt(boot0, "kubectl", "logs", "-n", "meows", "-l", "app.kubernetes.io/component=slack-agent", "|", "grep", "'success to send slack message'", "|", "grep", "-q", runnerPodList.Items[0].Name)
-			if err != nil {
-				return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
-			}
-			return nil
-		}).ShouldNot(HaveOccurred())
+		By("checking network policy for runner pods in a tenant namespace (" + tenantDevNS + ")")
+		connectionShouldBeDenied(tenantDevNS, "deploy/"+runnerPoolName, "http://argocd-server.argocd.svc")
+		connectionShouldBeDenied(tenantDevNS, "deploy/"+runnerPoolName, "http://slack-agent.meows.svc")
+		connectionShouldBeAllowed(tenantDevNS, "deploy/"+runnerPoolName, "https://kubernetes.default.svc")
 	})
+}
+
+func genRunnerPoolName() string {
+	hostname, err := os.Hostname()
+	ExpectWithOffset(1, err).NotTo(HaveOccurred())
+	return "runnerpool-" + hostname
+}
+
+func checkRunnerPool(name, namespace string) {
+	EventuallyWithOffset(1, func() error {
+		return checkDeploymentReplicas(name, namespace, 1)
+	}).Should(Succeed())
+
+	By("checking that runner pods become online")
+	query := `count(meows_runner_online{runnerpool="` + namespace + "/" + name + `"})`
+	EventuallyWithOffset(1, func() error {
+		result, err := queryMetrics(MonitoringLargeset, query)
+		if err != nil {
+			return err
+		}
+		if len(result.Data.Result) <= 0 {
+			return fmt.Errorf("no count metrics is retrieved")
+		}
+		onlineRunners := int(result.Data.Result[0].Value)
+		if onlineRunners != 1 {
+			return fmt.Errorf("there is not an online runner pod, the metrics of %s is %d", query, onlineRunners)
+		}
+		return nil
+	}).Should(Succeed())
+}
+
+func connectionShouldBeAllowed(namespace, from, to string) {
+	EventuallyWithOffset(1, func() error {
+		stdout, stderr, err := ExecAt(boot0, "kubectl", "exec", "-n", namespace, from, "--", "curl", "-k", "-sS", "-m5", to)
+		if err != nil {
+			return fmt.Errorf("stdout: %s, stderr: %s, err: %v", stdout, stderr, err)
+		}
+		return nil
+	}).Should(Succeed())
+}
+
+func connectionShouldBeDenied(namespace, from, to string) {
+	// When the connection is denied by NetworkPolicy, curl will time out with the following error message.
+	// > curl: (28) Connection timed out after 5000 milliseconds
+	// > command terminated with exit code 28
+	EventuallyWithOffset(1, func() error {
+		stdout, stderr, err := ExecAt(boot0, "kubectl", "exec", "-n", namespace, from, "--", "curl", "-k", "-sS", "-m5", to)
+		if err == nil {
+			return fmt.Errorf("connection is allowed, stdout: %s, stderr: %s", stdout, stderr)
+		}
+		if !strings.Contains(string(stderr), "curl: (28) Connection timed out") {
+			return fmt.Errorf("curl command is not timed out; stdout: %s, stderr: %s", stdout, stderr)
+		}
+		return nil
+	}).Should(Succeed())
 }
