69bba58fbf5905fc431ad2fbb74af018ea2cee4f
Hsn723 <antoine@cybozu.co.jp>
2022-03-03 04:11:41 +0000

    move crd removal to a later phase
    Signed-off-by: Hsn723 <antoine@cybozu.co.jp>

--- test/setup_test.go
+++ test/setup_test.go
@@ -438,24 +438,6 @@ func applyAndWaitForApplications(commitID string) {
 		}
 	}
 
-	//TODO: remove this block after releasing
-	// https://github.com/cybozu-go/neco-apps/pull/2321
-	if doUpgrade {
-		tenetCRDs := []string{
-			"networkpolicytemplates.tenet.cybozu.io",
-			"networkpolicyadmissionrules.tenet.cybozu.io",
-		}
-		for _, crd := range tenetCRDs {
-			_, _, err := ExecAt(boot0, "kubectl", "get", "crd", crd)
-			if err == nil {
-				stdout, stderr, err := ExecAt(boot0, "kubectl", "annotate", "crd", crd, fmt.Sprintf("admission.cybozu.com/i-am-sure-to-delete=%s", crd))
-				Expect(err).ShouldNot(HaveOccurred(), "failed to annotate tenet crd: stdout=%s, stderr=%s", stdout, stderr)
-				stdout, stderr, err = ExecAt(boot0, "kubectl", "delete", "crd", crd)
-				Expect(err).ShouldNot(HaveOccurred(), "failed to delete tenet crd: stdout=%s, stderr=%s", stdout, stderr)
-			}
-		}
-	}
-
 	By("syncing argocd-config")
 	Eventually(func() error {
 		stdout, stderr, err := ExecAt(boot0, "cd", "./neco-apps",
@@ -578,6 +560,24 @@ func applyAndWaitForApplications(commitID string) {
 			return fmt.Errorf("%s is not initialized. argocd app get %s -o json: %s", target.name, target.name, appStdout)
 		}
 		return nil
+	}
+
+	//TODO: remove this block after releasing
+	// https://github.com/cybozu-go/neco-apps/pull/2321
+	if doUpgrade {
+		tenetCRDs := []string{
+			"networkpolicytemplates.tenet.cybozu.io",
+			"networkpolicyadmissionrules.tenet.cybozu.io",
+		}
+		for _, crd := range tenetCRDs {
+			_, _, err := ExecAt(boot0, "kubectl", "get", "crd", crd)
+			if err == nil {
+				stdout, stderr, err := ExecAt(boot0, "kubectl", "annotate", "crd", crd, fmt.Sprintf("admission.cybozu.com/i-am-sure-to-delete=%s", crd))
+				Expect(err).ShouldNot(HaveOccurred(), "failed to annotate tenet crd: stdout=%s, stderr=%s", stdout, stderr)
+				stdout, stderr, err = ExecAt(boot0, "kubectl", "delete", "crd", crd)
+				Expect(err).ShouldNot(HaveOccurred(), "failed to delete tenet crd: stdout=%s, stderr=%s", stdout, stderr)
+			}
+		}
 	}
 
 	// want to do like "Eventually( Consistently(checkAllAppsSynced, 10sec, 1sec) )"
